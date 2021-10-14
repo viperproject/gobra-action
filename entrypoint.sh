12e2847445132af77d4e7e65777ca820a136c5fa
Johannes Gasser <johannes.gasser@accelerom.com>
2021-10-14 16:13:43 +0200

    added default build arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Creating a docker image with Gobra image tag: $INPUT_IMAGEVERSION"
-docker build -t docker-action --build-arg image_version="$INPUT_IMAGEVERSION" /docker-action
+docker build -t docker-action --build-arg "image_version=$INPUT_IMAGEVERSION" /docker-action
 
 echo "Run Docker Action container"
 docker run -e INPUT_CACHING -e INPUT_PROJECTLOCATION -e INPUT_PACKAGELOCATION -e INPUT_PACKAGES -e INPUT_JAVAXSS -e INPUT_JAVAXMX -e INPUT_GLOBALTIMEOUT -e INPUT_PACKAGETIMEOUT \
