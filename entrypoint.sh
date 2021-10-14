93e2306c0917f47f958152a55877e38fbb961457
Johannes Gasser <johannes.gasser@accelerom.com>
2021-10-14 16:03:29 +0200

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Creating a docker image with Gobra image tag: $INPUT_IMAGEVERSION"
 docker build -t docker-action --build-arg image_version="$INPUT_IMAGEVERSION" /docker-action
@@ -7,3 +7,4 @@ echo "Run Docker Action container"
 docker run -e INPUT_CACHING -e INPUT_PROJECTLOCATION -e INPUT_PACKAGELOCATION -e INPUT_PACKAGES -e INPUT_JAVAXSS -e INPUT_JAVAXMX -e INPUT_GLOBALTIMEOUT -e INPUT_PACKAGETIMEOUT \
   -v $GITHUB_WORKSPACE:$GITHUB_WORKSPACE \
   --workdir $GITHUB_WORKSPACE docker-action
+
