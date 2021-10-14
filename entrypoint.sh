a507f650b00cf387c94d94732c66e6687d00a6dc
Johannes Gasser <johannes.gasser@accelerom.com>
2021-10-14 15:53:01 +0200

    moved path to docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd /docker-action
-
 echo "Creating a docker image with Gobra image tag: $INPUT_IMAGEVERSION"
-docker build -t docker-action --build-arg image_version="$INPUT_IMAGEVERSION" .
+docker build -t docker-action --build-arg image_version="$INPUT_IMAGEVERSION" /docker-action
 
 echo "Run Docker Action container"
 docker run -e INPUT_CACHING -e INPUT_PROJECTLOCATION -e INPUT_PACKAGELOCATION -e INPUT_PACKAGES -e INPUT_JAVAXSS -e INPUT_JAVAXMX -e INPUT_GLOBALTIMEOUT -e INPUT_PACKAGETIMEOUT \
