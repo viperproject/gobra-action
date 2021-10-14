6b4220c8ffcdf44cf4aa26ae4ebe9a15712aa4af
Johannes Gasser <johannes.gasser@accelerom.com>
2021-10-14 17:33:58 +0200

    try to switch mounting directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Creating a docker image with Gobra image tag: $INPUT_IMAGEVERSION"
 docker build -t docker-action --build-arg "image_version=$INPUT_IMAGEVERSION" /docker-action
 
 echo "Run Docker Action container"
-docker run -e INPUT_CACHING -e INPUT_PROJECTLOCATION -e INPUT_PACKAGELOCATION -e INPUT_PACKAGES -e INPUT_JAVAXSS -e INPUT_JAVAXMX -e INPUT_GLOBALTIMEOUT -e INPUT_PACKAGETIMEOUT -e GITHUB_WORKSPACE -e GITHUB_REPOSITORY="/gobra/project" \
+docker run -e INPUT_CACHING -e INPUT_PROJECTLOCATION -e INPUT_PACKAGELOCATION -e INPUT_PACKAGES -e INPUT_JAVAXSS -e INPUT_JAVAXMX -e INPUT_GLOBALTIMEOUT -e INPUT_PACKAGETIMEOUT -e GITHUB_WORKSPACE="/gobra/project" -e GITHUB_REPOSITORY \
   -v $GITHUB_WORKSPACE:"/gobbra/project" \
   --workdir $GITHUB_WORKSPACE docker-action
 
