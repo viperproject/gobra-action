d1838ded38de544f8eed02ce8f4a4615579f3904
João Pereira <joaopereira.19@gmail.com>
2022-05-05 10:39:08 +0200

    test with paths with spaces

--- test/include path/pkg2/file1.go
+++ test/include path/pkg2/file1.go
@@ -1,14 +1,6 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
-package main
+package pkg2
 
-import (
-	"pkg1"
-	"pkg2"
-)
-
-func main() {
-	pkg1.F()
-	pkg2.G()
-}
+func G() int
