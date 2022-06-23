60f3e0389f546358c7a1c4f1560f17cc8b953543
João Pereira <joaopereira.19@gmail.com>
2022-06-23 13:42:41 +0200

    Incorporate feedback from ShellCheck

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -21,11 +21,11 @@ REPOSITORY_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}' | sed -e "s/
 # absolute path. Note: does not handle paths that contain a space.
 getFileListInDir () (
     local LOCATION=$1
-    cd -- "$LOCATION"
+    cd -- "$LOCATION" || exit
     # the tail of the list of arguments (i.e., the args without
     # the function name and the first argument (LOCATION) are
     # the list of paths to be processed.
-    echo "$(echo "${@:2}" | xargs realpath | tr '\n' ' ')"
+    "$(echo "${@:2}" | xargs realpath | tr '\n' ' ')"
 )
 
 GOBRA_JAR="/gobra/gobra.jar"
@@ -43,7 +43,7 @@ if [[ $INPUT_RECURSIVE -eq 1 ]]; then
 fi
 
 if [[ $INPUT_FILES ]]; then
-    RESOLVED_PATHS="$(getFileListInDir $PROJECT_LOCATION $INPUT_FILES)"
+    RESOLVED_PATHS="$(getFileListInDir "$PROJECT_LOCATION" "$INPUT_FILES")"
     echo "[DEBUG] Project Location: $PROJECT_LOCATION" > $DEBUG_OUT
     echo "[DEBUG] Input Files: $INPUT_FILES" > $DEBUG_OUT
     echo "[DEBUG] Resolved Paths: $RESOLVED_PATHS" > $DEBUG_OUT
@@ -52,12 +52,12 @@ fi
 
 if [[ $INPUT_PACKAGES ]]; then
     # INPUT_PACKAGES are paths to packages
-    RESOLVED_PATHS="$(getFileListInDir $PROJECT_LOCATION $INPUT_PACKAGES)"
+    RESOLVED_PATHS="$(getFileListInDir "$PROJECT_LOCATION" "$INPUT_PACKAGES")"
     GOBRA_ARGS="-p $RESOLVED_PATHS $GOBRA_ARGS"
 fi
 
 if [[ $INPUT_INCLUDEPATHS ]]; then
-    RESOLVED_PATHS=$(getFileListInDir $PROJECT_LOCATION $INPUT_INCLUDEPATHS)
+    RESOLVED_PATHS=$(getFileListInDir "$PROJECT_LOCATION" "$INPUT_INCLUDEPATHS")
     echo "[DEBUG] Project Location: $PROJECT_LOCATION" > $DEBUG_OUT
     echo "[DEBUG] Include Paths: $INPUT_INCLUDEPATHS" > $DEBUG_OUT
     echo "[DEBUG] Resolved Paths: $RESOLVED_PATHS" > $DEBUG_OUT
@@ -105,9 +105,9 @@ EXIT_CODE=0
 
 CMD="java $JAVA_ARGS $GOBRA_ARGS"
 
-echo $CMD
+echo "$CMD"
 
-if timeout $INPUT_GLOBALTIMEOUT $CMD; then
+if timeout "$INPUT_GLOBALTIMEOUT" "$CMD"; then
     echo -e "${GREEN}Verification completed successfully${RESET}"
 else
     EXIT_CODE=$?
@@ -118,7 +118,7 @@ else
     fi
 fi
 
-TIME_PASSED=$[ $SECONDS-$START_TIME ]
+TIME_PASSED=$((SECONDS-START_TIME))
 
 echo "::set-output name=time::$TIME_PASSED"
 
